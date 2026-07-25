5e2c1fdb7366c3d8e475d1a7a06c4f6ec3fdf02e
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-25 09:31:51 -0400

    Add Master Customers access to control plane layout

--- app/master/layout.tsx
+++ app/master/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import PlatformRegistrationAction from "@/components/master/PlatformRegistrationAction";
+import { MasterCustomersShortcut } from "@/components/master/MasterCustomersShortcut";
 
 export default function MasterLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PlatformRegistrationAction />
+      <MasterCustomersShortcut />
       {children}
     </>
   );
