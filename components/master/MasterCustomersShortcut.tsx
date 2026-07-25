bfb80f3e699cf7109ba28d020c3c0d3b8bd25643
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-25 09:31:23 -0400

    Style Master Customers navigation shortcut

--- components/master/MasterCustomersShortcut.tsx
+++ components/master/MasterCustomersShortcut.tsx
@@ -6,5 +6,5 @@ import { usePathname } from "next/navigation";
 export function MasterCustomersShortcut() {
   const pathname = usePathname();
   if (pathname === "/master/customers") return null;
-  return <Link href="/master/customers" className="master-customers-shortcut">Customers</Link>;
+  return <Link href="/master/customers" style={{position:"fixed",left:20,bottom:22,zIndex:90,padding:"11px 16px",borderRadius:12,background:"#0b5c43",color:"#fff",fontWeight:800,textDecoration:"none",boxShadow:"0 12px 28px rgba(4,61,46,.25)"}}>Customers</Link>;
 }
