d8736f4ae8ab1a71218af3f85a621451523aab66
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-26 13:30:16 -0400

    Load shared role mobile design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./role-portal-alignment.css";
 import "./customer-profile-media.css";
 import "./role-profile-media.css";
+import "./role-mobile-system.css";
 import {SeasonThemeProvider} from "@/components/SeasonThemeProvider";
 import {AdminAccessFallback} from "@/components/master/AdminAccessFallback";
 import {CustomerLegacyDataGuard} from "@/components/customer/CustomerLegacyDataGuard";
