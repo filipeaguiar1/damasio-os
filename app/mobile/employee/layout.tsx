69f0be4997e313c3981ab897df7282dcf0a44531
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-26 15:12:06 -0400

    Use dedicated employee home navigation without duplicate layout

--- app/mobile/employee/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PremiumEmployeeNav } from "@/components/mobile/PremiumEmployeeNav";
-
-export default function MobileEmployeeLayout({children}:{children:React.ReactNode}){
-  return <>{children}<PremiumEmployeeNav/></>;
-}
