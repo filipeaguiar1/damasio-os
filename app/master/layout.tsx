ac47d51d97417b806a997e8e429708841d881d70
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-26 11:05:03 -0400

    Expose Master payments workspace

--- app/master/layout.tsx
+++ app/master/layout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 import PlatformRegistrationAction from "@/components/master/PlatformRegistrationAction";
 import { MasterCustomersShortcut } from "@/components/master/MasterCustomersShortcut";
+import { MasterPaymentsShortcut } from "@/components/master/MasterPaymentsShortcut";
 
 export default function MasterLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PlatformRegistrationAction />
       <MasterCustomersShortcut />
+      <MasterPaymentsShortcut />
       {children}
     </>
   );
