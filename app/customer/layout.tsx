96591e4855784f2f7330389b749d818c19c7fcad
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-07-26 10:21:23 -0400

    Load Payments and Visits styles

--- app/customer/layout.tsx
+++ app/customer/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
 import {RoleGuard} from "@/components/auth/RoleGuard";
 import "./customer-services.css";
+import "./payments-visits.css";
 export default function CustomerLayout({children}:{children:React.ReactNode}){return <RoleGuard allowed={["customer"]}>{children}</RoleGuard>}
