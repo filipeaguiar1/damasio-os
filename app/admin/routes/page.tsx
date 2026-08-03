9836766117da1703b08da014646d6206ca84343a
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-08-03 13:29:22 -0400

    feat(routes): expose pending removal action in advisor

--- app/admin/routes/page.tsx
+++ app/admin/routes/page.tsx
@@ -2,7 +2,8 @@
 
 import { AdminShell } from "@/components/admin/AdminShell";
 import { RouteStudio } from "@/components/admin/RouteStudio";
+import { RouteTodayRemovalPanel } from "@/components/admin/RouteTodayRemovalPanel";
 
 export default function RoutesPage(){
-  return <AdminShell active="Routes"><RouteStudio/></AdminShell>;
+  return <AdminShell active="Routes"><RouteStudio/><RouteTodayRemovalPanel/></AdminShell>;
 }
