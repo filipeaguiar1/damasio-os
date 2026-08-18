0ad41348bbf4896818bab94ed00382ca30048956
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-08-17 20:01:06 -0400

    Fix no-date transfer and polish route experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./role-mobile-system.css";
 import "./customer-mobile-overnight.css";
 import "./e2e-surface-fixes.css";
 import "./route-build-polish.css";
+import "./route-experience-polish.css";
 import {SeasonThemeProvider} from "@/components/SeasonThemeProvider";
 import {AdminAccessFallback} from "@/components/master/AdminAccessFallback";
 import {CustomerLegacyDataGuard} from "@/components/customer/CustomerLegacyDataGuard";
