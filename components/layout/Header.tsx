8d6a122b6237bafd714be24fb1c723e165167808
filipeaguiar1 <filipeaguiartt@gmail.com>
2026-08-22 12:09:38 -0400

    Route header customer link through login

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
           <a href="/#plans">Plans</a>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
-          <Link href="/customer">Customer Portal</Link>
+          <Link href="/login">Customer Portal</Link>
         </nav>
         <Link className="btn btn-primary header-login" href="/login">
           <span className="header-login-desktop">Sign in</span>
